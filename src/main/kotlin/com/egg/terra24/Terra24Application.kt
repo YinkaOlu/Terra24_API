5278e0844298d4940254d2ff372072cd329f196c
yinka <yinka.olu1@gmail.com>
2020-11-20 18:55:49 -0500

    init

---
+++ src/main/kotlin/com/egg/terra24/Terra24Application.kt
@@ -0,0 +1,11 @@
+package com.egg.terra24
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Terra24Application
+
+fun main(args: Array<String>) {
+	runApplication<Terra24Application>(*args)
+}
---
+++ src/test/kotlin/com/egg/terra24/Terra24ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.egg.terra24
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Terra24ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
