658b811eebefbb61ec3df8706024a21b72ce64be
yinka <yinka.olu1@gmail.com>
2020-12-06 18:25:18 -0500

    - add database config to connect to heroku database

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.postgresql:postgresql")
+
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
