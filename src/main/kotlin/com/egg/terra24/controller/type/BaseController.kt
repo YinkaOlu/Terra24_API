fd205a9cddb8baf9bbe8aae6ec122047ef4b9d5c
yinka <yinka.olu1@gmail.com>
2020-12-19 20:01:10 -0500

    Allow Cors

--- src/main/kotlin/com/egg/terra24/controller/type/BaseController.kt
+++ src/main/kotlin/com/egg/terra24/controller/type/BaseController.kt
@@ -1,6 +1,9 @@
 package com.egg.terra24.controller.type
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.cors.CorsConfiguration
 
 @RequestMapping("/api/v1")
+@CrossOrigin(value = [CorsConfiguration.ALL])
 interface BaseController
